a7138d87635ffd52f3ff77c50712565b3e36ea0c
AnotherDebug <antonio.faddanno@gmail.com>
2023-10-27 16:18:58 +0200

    [ADD]: layout

--- index.php
+++ index.php
@@ -1,4 +1,3 @@
-
 <?php
 
 
@@ -8,13 +7,50 @@
 
 <!DOCTYPE html>
 <html lang="it">
-    <head>
-        <title>PHP Strong Password Generator</title>
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <link href="css/style.css" rel="stylesheet">
-    </head>
-    <body>
-    
-    </body>
+
+<head>
+    <title>PHP Strong Password Generator</title>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- Bootstrap -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-4bw+/aepP/YC94hEpVNVgiZdgIC5+VKNBQNGCHeKRQN+PtmoHDEXuppvnDJzQIu9" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-HwwvtgBNo3bZJJLYd8oVXjrBZt8cqVSpeBNS5n7C8IVInixGAoxmnlMuBnhbgrkm" crossorigin="anonymous"></script>
+
+    <link href="css/style.css" rel="stylesheet">
+</head>
+
+<body class="bg-black">
+
+    <header>
+        <h1 class="text-center mt-5 text-info">STRONG PASS GENERATOR</h1>
+    </header>
+
+    <main>
+        <h3 class="text-center text-light ">Genera una password sicura</h3>
+        <div class="container">
+            <div class="message bg-info">
+                <p class="lh-5 p-3 ">Messaggio di prova</p>
+            </div>
+            <div class="genPassBox bg-light ">
+                <div class="row d-flex align-items-center p-3 ">
+                    <div class="col-6">
+                        <p class="fw-bold">Lunghezza Password:</p>
+                    </div>
+                    <div class="col-6">
+                        <div class="mb-3">
+                            <input type="text" class="form-control w-50 " id="text" placeholder="Inserisci un numero.">
+                        </div>
+                    </div>
+                    <div class="col">
+                    <button type="submit" class="btn btn-primary">Invia</button>
+                    <button type="button" class="btn btn-secondary">Annulla</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </main>
+
+</body>
+
 </html>
