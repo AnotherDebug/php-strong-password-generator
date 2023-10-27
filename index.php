a8c81a9d82d5fd145eeda5920cf1173abb9b0050
AnotherDebug <antonio.faddanno@gmail.com>
2023-10-27 15:32:22 +0200

    [INIT]: file index

---
+++ index.php
@@ -0,0 +1,20 @@
+
+<?php
+
+
+
+
+?>
+
+<!DOCTYPE html>
+<html lang="it">
+    <head>
+        <title>PHP Strong Password Generator</title>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link href="css/style.css" rel="stylesheet">
+    </head>
+    <body>
+    
+    </body>
+</html>
