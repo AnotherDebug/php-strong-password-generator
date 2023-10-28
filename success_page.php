bb9da009e5faf723c05c1782b5d19c2e516c2eb4
AnotherDebug <antonio.faddanno@gmail.com>
2023-10-28 12:40:47 +0200

    [ADD]: success page

---
+++ success_page.php
@@ -0,0 +1,54 @@
+<?php
+
+require_once __DIR__ . '/data/functions.php';
+
+$upper_list = "QWERTYUIOPASDFGHJKLZXCVBNM";
+$lower_list = "qwertyuiopasdfghjklzxcvbnm";
+$number_list = "1234567890";
+$special_chars_list = "!@#$%^&*-_?><.;/|";
+$characters_list = $upper_list . $lower_list . $number_list . $special_chars_list;
+
+
+session_start();
+
+if (isset($_SESSION['send_pass'])) {
+    $pass = $_SESSION['send_pass'];
+    $gen_pass = genPass($pass, $characters_list);
+}else{
+    header('location: ./index.php');
+}
+
+
+
+require_once __DIR__ . '/partials/head.php';
+
+?>
+
+
+<body class="bg-black">
+
+
+    <main class="pt-5">
+        <h3 class="text-center text-light ">La password generata è:</h3>
+        <div class="container">
+            <div class="message bg-info">
+                <p class="lh-5 p-3 text-center"><?php echo $gen_pass ?></p>
+            </div>
+            <div class="genPassBox bg-light ">
+
+                <!-- FORM -->
+                <form action="index.php" method="POST">
+                    <div class="row d-flex align-items-center p-3 ">
+                        <div class="col">
+                            <a class="btn btn-secondary " href="index.php">Torna indietro</a>
+                        </div>
+                    </div>
+                </form>
+
+            </div>
+        </div>
+    </main>
+
+</body>
+
+</html>
