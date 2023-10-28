6f2d27c4708c09b76d7ee733b4aaf64dab76628d
AnotherDebug <antonio.faddanno@gmail.com>
2023-10-28 12:46:00 +0200

    [ADD]: file data variables

---
+++ data/variables.php
@@ -0,0 +1,7 @@
+<?php
+
+$upper_list = "QWERTYUIOPASDFGHJKLZXCVBNM";
+$lower_list = "qwertyuiopasdfghjklzxcvbnm";
+$number_list = "1234567890";
+$special_chars_list = "!@#$%^&*-_?><.;/|";
+$characters_list = $upper_list . $lower_list . $number_list . $special_chars_list;
